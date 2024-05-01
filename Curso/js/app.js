9ad9b14d79c3e1958b3d4257a3524c07af4da8e0
Daniel Grande <mhayhemdev@gmail.com>
2024-05-01 15:40:45 +0100

    added regex

--- Curso/js/app.js
+++ Curso/js/app.js
@@ -3,19 +3,20 @@
 const nickInput = document.getElementById('nick');
 const sizeInput = document.getElementById('size');
 const formElement = document.getElementById('formElement');
+const error = document.getElementById('error');
 
 // events function 
 
 function formCheck(event) {
-    if (nickInput.value.length == 0) {
-        console.log('No hay nick');
+    if (nickInput.value.match(/(?<!\S)[0-9]/)) {
         event.preventDefault();
+        error.innerText='el nombre no puede empezar con un numero';
         nickInput.focus();
         return false;
     }
     if (sizeInput.value == '0') {
-        console.log('No se ha seleccionado tamaño');
         event.preventDefault();
+        error.innerText='Seleccione un tamaño de juego';
         sizeInput.focus();
         return false;
     }
