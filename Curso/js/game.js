29ff7d6a11d3168271386110acbdcb958331244f
Daniel Grande <mhayhemdev@gmail.com>
2024-07-03 17:23:36 +0100

    added feature to marck background of dots

--- Curso/js/game.js
+++ Curso/js/game.js
@@ -1,25 +1,61 @@
 
+
 // game code
 
+/**
+ * Description
+ * @param {int} max
+ * @returns {int} random number
+ */
 function getRamdomInt(max) {
     return Math.floor(Math.random() * max);
 }
+/**
+ * Description
+ * complete the form from sessionStorage
+ */
 function fillUserForm() {
-    document.getElementById("nick").value=nick;
-    document.getElementById("avatarImg").src=avatarImg;
+    document.getElementById('nick').value=nick;
+    document.getElementById('avatarImg').src=avatarImg;
 }
+/**
+ * Description
+ * display the game panel
+ */
 function displayGameLayout() {
-    document.getElementById("game").style.gridTemplateColumns="repeat("+size+", 1fr)";
-    document.getElementById("game").style.gridTemplateRows="repeat("+size+", 1fr)";
+    document.getElementById('game').style.gridTemplateColumns="repeat("+size+", 1fr)";
+    document.getElementById('game').style.gridTemplateRows="repeat("+size+", 1fr)";
     
     let items = ""
-    let color = ["red", "green"];
+    let color = ["red", "green", "blue"];
     let colorRdm = 0;
     for (let index = 0; index < (parseInt(size)*parseInt(size)); index++) {
-        if (index % 2 != 0) colorRdm = getRamdomInt(2);
+        if (index % 2 > 0) colorRdm = getRamdomInt(3);
         items+=`<div class="containerItem"><div class="item ${color[colorRdm]}"></div></div>`;
     }
-    document.getElementById("game").innerHTML=items;
+    document.getElementById('game').innerHTML=items;
+}
+/**
+ * Description
+ * start the mouse down event
+ */
+function scheduleGameEvents() {
+    const items = document.getElementsByClassName('item');
+    for (let item of items) {
+        item.addEventListener('mousedown', startMark);
+    }
+}
+/**
+ * Description
+ * @param {event} 
+ * star to mark the dots
+ */
+function startMark(event) {
+    let item = event.target;
+    let containerItem = event.target.parentElement;
+    if (item.classList.contains("red")) containerItem.classList.add("red");
+    else if (item.classList.contains("blue")) containerItem.classList.add("blue");
+    else containerItem.classList.add("green");
 }
 // take user data
 getUserData();
@@ -29,5 +65,6 @@ if (!checkUserData()) location="index.html";
 // fill the form
 fillUserForm();
 displayGameLayout();
+scheduleGameEvents();
 
 
