a4d150a54e668917feff9b304a8a8887222b14fc
Daniel Grande <mhayhemdev@gmail.com>
2024-07-03 14:04:57 +0100

    added new features

--- Curso/js/game.js
+++ Curso/js/game.js
@@ -1,6 +1,9 @@
 
 // game code
 
+function getRamdomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 function fillUserForm() {
     document.getElementById("nick").value=nick;
     document.getElementById("avatarImg").src=avatarImg;
@@ -8,6 +11,15 @@ function fillUserForm() {
 function displayGameLayout() {
     document.getElementById("game").style.gridTemplateColumns="repeat("+size+", 1fr)";
     document.getElementById("game").style.gridTemplateRows="repeat("+size+", 1fr)";
+    
+    let items = ""
+    let color = ["red", "green"];
+    let colorRdm = 0;
+    for (let index = 0; index < (parseInt(size)*parseInt(size)); index++) {
+        if (index % 2 != 0) colorRdm = getRamdomInt(2);
+        items+=`<div class="containerItem"><div class="item ${color[colorRdm]}"></div></div>`;
+    }
+    document.getElementById("game").innerHTML=items;
 }
 // take user data
 getUserData();
